bc29a625d0d5a53660a7b162e04b7da9a9095bf1
naim <naiim.chatti@gmail.com>
2026-04-13 00:22:45 +0800

    Feat(P1-T10): use db and prisma to do the CRUD operations

--- phase-1/project-1/src/posts/posts.router.ts
+++ phase-1/project-1/src/posts/posts.router.ts
@@ -5,52 +5,61 @@ import { CreatePostSchema, IDSchema, UpdatePostSchema } from './posts.schema';
 
 const postsRouter = express.Router();
 
-postsRouter.get('/', (_req: Request, res: Response) => {
-	const posts = listPosts()
+postsRouter.get('/', async (_req: Request, res: Response) => {
+	const posts = await listPosts()
 
 	res.json(posts);
 });
 
-postsRouter.get('/:id', validateParamsSchema(IDSchema), (req: Request, res: Response, next: NextFunction) => {
+postsRouter.get('/:id', validateParamsSchema(IDSchema), async (req: Request, res: Response, next: NextFunction) => {
 	const { id } = req.params;
 
 	try {
-		const posts = getPost(Number(id))
+		const posts = await getPost(Number(id))
 		res.json(posts);
 	} catch (error) {
 		next(error)
 	}
 });
 
-postsRouter.post('/', validateBodySchema(CreatePostSchema) ,(req: Request, res: Response, next: NextFunction) => {
-	const data = req.body;
-	
-	try {
-		const createPost = addPost(data)
+postsRouter.post(
+	'/',
+	validateBodySchema(CreatePostSchema), 
+	async (req: Request, res: Response, next: NextFunction) => {
+		const data = req.body;
+		const userId = 1; // need to be changed by the current connected user.
+		
+		try {
+			const createPost = await addPost(data, userId)
 
-		res.status(201).json(createPost);
-	} catch (error) {
-		next(error)
+			res.status(201).json(createPost);
+		} catch (error) {
+			next(error)
+		}
 	}
-});
+);
 
-postsRouter.patch('/:id', validateParamsSchema(IDSchema), validateBodySchema(UpdatePostSchema), (req: Request, res: Response, next: NextFunction) => {
-	const data = req.body;
-	const { id } = req.params;
-	try {
-		const updatedPost = updatePost(Number(id), data)
+postsRouter.patch('/:id', 
+	validateParamsSchema(IDSchema), 
+	validateBodySchema(UpdatePostSchema), 
+	async (req: Request, res: Response, next: NextFunction) => {
+		const data = req.body;
+		const { id } = req.params;
+		try {
+			const updatedPost = await updatePost(Number(id), data)
 
-		res.json(updatedPost);
-	} catch (error) {
-		next(error)
+			res.json(updatedPost);
+		} catch (error) {
+			next(error)
+		}
 	}
-});
+);
 
-postsRouter.delete('/:id', validateParamsSchema(IDSchema), (req: Request, res: Response, next: NextFunction) => {
+postsRouter.delete('/:id', validateParamsSchema(IDSchema), async(req: Request, res: Response, next: NextFunction) => {
 	const { id } = req.params;
 
 	try {
-		deletePost(Number(id))
+		await deletePost(Number(id))
 
 		res.status(204).send();
 	} catch (error) {
--- phase-1/project-1/src/utils/posts.ts
+++
@@ -1,3 +0,0 @@
-import { POSTS } from "../posts/posts.service"
-
-export const findPostIndex = (id: number) => POSTS.findIndex(post => post.id === id)
